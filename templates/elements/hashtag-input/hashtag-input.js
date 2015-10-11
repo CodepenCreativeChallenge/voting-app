bc530fc27e16d4f52a2a93f7a8abb758ff19e359
Haroen Viaene <hello@haroen.me>
2015-10-12 00:14:20 +0200

    more bad code

--- templates/elements/hashtag-input/hashtag-input.js
+++ templates/elements/hashtag-input/hashtag-input.js
@@ -1,23 +1,6 @@
 (function() {
   'use strict';
   Polymer({
-    is: 'hashtag-input',
-    properties: {
-      username: {
-        type: String,
-        value: 'pixelass'
-      },
-      'realname': {
-        type: String,
-        value: 'Gregor Adams'
-      },
-      url: {
-        type: String,
-        computed: 'userPath(username)'
-      }
-    },
-    userPath: function userPath(username) {
-      return `users/${username}`;
-    }
+    is: 'hashtag-input'
   });
 })();
