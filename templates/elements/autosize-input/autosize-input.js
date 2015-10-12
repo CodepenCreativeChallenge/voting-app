c3c426421b8a780f4a536ae9d0887d8b240f98cc
Gregor Adams <greg@pixelass.com>
2015-10-12 02:58:54 -0700

    adjust styles and script

--- templates/elements/autosize-input/autosize-input.js
+++ templates/elements/autosize-input/autosize-input.js
@@ -4,7 +4,6 @@
     is: 'autosize-input',
     attached: function() {
       var input = this.querySelector('input');
-      input.style.width = '10px';
       var dummy = document.createElement('span');
       dummy.style.position = 'absolute';
       dummy.style.left = '-10000em';
@@ -12,15 +11,18 @@
       var body = document.body;
       body.appendChild(dummy);
       var computed = window.getComputedStyle(input);
-      [
-      'font-size',
-      'font-style',
-      'font-weight',
-      'font-family',
-      'line-height',
-      'text-transform',
-      'letter-spacing'
-      ].forEach(function(prop) {
+      var props = [
+        'fontSize',
+        'fontStyle',
+        'fontWeight',
+        'fontFamily',
+        'padding',
+        'lineHeight',
+        'textTransform',
+        'letterSpacing'
+      ];
+
+      props.forEach((prop) => {
         dummy.style[prop] = computed[prop];
       });
 
@@ -29,10 +31,12 @@
 
       input.addEventListener('input', (e) => {
         dummy.innerText = e.target.value;
-        input.style.width = dummy.offsetWidth + 'px';
+        let dummyWitdth = window.getComputedStyle(dummy).width;
+        input.style.width = dummyWitdth;
 
         if (e.target.value.length < 1) {
           dummy.innerText = input.placeholder;
+          let dummyWitdth = window.getComputedStyle(dummy).width;
           input.style.width = dummy.offsetWidth + 'px';
         }
       });
